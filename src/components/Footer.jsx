a99f9134d9db7e051417d4190c317478e5217df1
Rakyno <nikan.nikoufar2004@gmail.com>
2025-04-15 08:53:16 +0330

    few twiks here and there

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,14 @@
-import styles from '../styles/Footer.module.css';
+import styles from "../styles/Footer.module.css";
 
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <p> mock up blog | exporting from jsonplaceholder.</p>
+      <p>
+        {" "}
+        this is purely a mock up blog with nothing special. The data for this
+        very latin blog was imported from jsonplaceholder. Ive not put alot of
+        time into this except maybe that dark-m0de switch up there.
+      </p>
     </footer>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,24 @@
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import styles from '../styles/Header.module.css';
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import styles from "../styles/Header.module.css";
 
 function Header() {
   const [darkMode, setDarkMode] = useState(false);
 
   useEffect(() => {
-    document.body.classList.toggle('dark-mode', darkMode);
+    document.body.classList.toggle("dark-mode", darkMode);
   }, [darkMode]);
 
   return (
     <header className={styles.header}>
-      <h1><Link to="/">My Blog</Link></h1>
+      <h1>
+        <Link to="/">Nikan's latin blog</Link>
+      </h1>
       <label className={styles.toggleSwitch}>
         <input
           type="checkbox"
           checked={darkMode}
-          onChange={() => setDarkMode(prev => !prev)}
+          onChange={() => setDarkMode((prev) => !prev)}
         />
         <span className={styles.slider}></span>
       </label>
@@ -25,4 +27,3 @@ function Header() {
 }
 
 export default Header;
-
